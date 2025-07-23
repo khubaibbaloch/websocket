053f98b1ec2966e6a403c52158ca4871928f07cf
khubaibbaloch <balochkhubaib008@gmail.com>
2025-07-23 02:04:28 -0700

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+//    configureSecurity()
+//    configureHTTP()
+//    configureMonitoring()
+//    configureSerialization()
+
+    configureSockets()
+    configureRouting()
+    configureSerialization()
+
+}
